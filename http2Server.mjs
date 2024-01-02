d389503c79a83508982a231229813335851d36f5
Nitin Chaudhary <76252414+nitin-io@users.noreply.github.com>
2024-01-02 15:54:50 +0530

    Add Route handling with http2 server

--- http2Server.mjs
+++ http2Server.mjs
@@ -2,17 +2,33 @@ import http2 from "http2";
 
 const server = http2.createServer();
 
+const rootRouteHandler = (stream, headers) => {
+  stream.respond({
+    ":status": 200,
+  });
+  stream.end("Hello, World!");
+};
+
+const notFoundHandler = (stream, headers) => {
+  stream.respond({
+    ":status": 404,
+  });
+  stream.end("Route Not Found!");
+};
+
 server.on("error", (err) => {
   console.log(err.message);
 });
 
 server.on("stream", (stream, headers) => {
-  stream.respond({
-    ":status": 200,
-  });
-  stream.write("Hello, World!");
-  stream.end();
-  console.log(stream);
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  if (path === "/" && method === "GET") {
+    rootRouteHandler(stream, headers);
+  } else {
+    notFoundHandler(stream, headers);
+  }
 });
 
 server.listen(8080);
